da9494f29b23984ae6f02755accf3533cb65799d
Festus Ojo <festusojo@fb.com>
2019-06-27 17:29:55 -0700

    completed

---
+++ ViewControllers/DetailViewController.h
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.h
+//  Flix
+//
+//  Created by festusojo on 6/27/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- ViewControllers/MoviesViewController.m
+++ ViewControllers/MoviesViewController.m
@@ -9,12 +9,14 @@
 #import "MoviesViewController.h"
 #import "MovieCell.h"
 #import "UIImageView+AFNetworking.h"
+#import "DetailViewController.h"
 
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>;
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray *movies;
-@property (nonatomic, strong) UIRefreshControl *refreshControl
+@property (nonatomic, strong) UIRefreshControl *refreshControl; 
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *refreshIndicator;
 
 @end
 
@@ -27,8 +29,11 @@
     self.tableView.delegate = self;
 
     [self fetchMovies];
+    
+    [self.refreshIndicator startAnimating];
 
     self.refreshControl = [[UIRefreshControl alloc] init];
+    
     [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:(UIControlEventValueChanged)];
 [self.tableView insertSubview:self.refreshControl atIndex:0];
 }
@@ -57,8 +62,11 @@
             // TODO: Store the movies in a property to use elsewhere
             // TODO: Reload your table view data
         }
+        [self.refreshControl endRefreshing];
+        
     }];
     [task resume];
+    [self.refreshIndicator stopAnimating];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -86,10 +94,19 @@
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
+ */
+
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
+    // Get sthe new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    
+    UITableViewCell *tappedCell = sender;
+    NSIndexPath *indexPath = [self.tableView indexPathForCell:tappedCell];
+    NSDictionary *movie = self.movies[indexPath.row];
+    
+    DetailViewController *detailViewController = [segue destinationViewController];
+    detailViewController.movie = movie;
 }
-*/
+
 
 @end
